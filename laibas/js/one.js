3ebb27bf83860a929e54f0c9fb5c8eec8a1e374b
Mikhail8920 <other_vip@mail.ru>
2020-02-19 06:55:35 +0300

    liabas

--- laibas/js/one.js
+++ laibas/js/one.js
@@ -1,3 +1,29 @@
-// document.querySelector('.menu__button').onclick = function(header__nav) {
-//     header__nav.style.visibility = 'visible';
-// }
+document.getElementsByClassName('menu__button')[0].onclick = function() {
+    const navigation = document.getElementsByClassName('header__nav')[0];
+    // console.log(navigation)
+    console.log(navigation.classList);
+    [navigation.classList].indexOf('header__nav_open') == -1 ? navigation.classList.add('header__nav_open') : navigation.classList.remove('header__nav_open');
+    }
+
+    for (item of document.getElementsByClassName('header__nav__link')) {
+        item.onclick = function() {
+            const navigation = document.getElementsByClassName('header__nav')[0];
+            navigation.classList.remove('header__nav_open');
+        }
+        item.onclick = function() {
+            const navigation = document.getElementsByClassName('header__nav')[0];
+            navigation.classList.remove('header__nav_open');
+        }
+        item.onclick = function() {
+            const navigation = document.getElementsByClassName('header__nav')[0];
+            navigation.classList.remove('header__nav_open');
+        }
+        item.onclick = function() {
+            const navigation = document.getElementsByClassName('header__nav')[0];
+            navigation.classList.remove('header__nav_open');
+        }
+        item.onclick = function() {
+            const navigation = document.getElementsByClassName('header__nav')[0];
+            navigation.classList.remove('header__nav_open');
+        }
+    }
