ea48a801d75089d8419e613e33859ebb6c92d14b
Mikhail8920 <60809209+Mikhail8920@users.noreply.github.com>
2020-02-20 16:36:33 +0300

    Update one.js

--- laibas/js/one.js
+++ laibas/js/one.js
@@ -15,13 +15,13 @@ document.querySelector("#menu__toggle").onclick = function change2() {
     if ((document.querySelector(".header__nav").style.visibility == "hidden") && (off.checked == true)) {
         document.querySelector(".header__nav").style.visibility = "visible";
      alert("первый")
-    } else if ((document.querySelector(".header__nav").style.visibility == "visible") && (off.checked == true)) {
+    } else if ((document.querySelector(".header__nav").style.visibility == "visible") && (off.checked == false)) {
         document.querySelector(".header__nav").style.visibility = "hidden";
     alert("второй")
-    } //else if (off.checked == true) {
-        //document.querySelector(".header__nav").style.visibility = "visible";
-    //alert('третий')
-    //} 
+    } else if (off.checked == true) {
+        document.querySelector(".header__nav").style.visibility = "visible";
+    alert('третий')
+    } 
 }
 
     // for (item of document.getElementsByClassName('header__nav__link')) {
