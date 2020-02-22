71b08ec777dc5f19c0dc35f75a535b811f515b22
Mikhail8920 <other_vip@mail.ru>
2020-02-22 21:17:46 +0300

    liabas

--- laibas/js/one.js
+++ laibas/js/one.js
@@ -1,45 +1,24 @@
-// document.getElementsByClassName('menu__button')[0].onclick = function() {
-//     const navigation = document.getElementsByClassName('header__nav')[0];
-//      console.log(navigation)
-//     console.log(navigation.classList);
-//     [navigation.classList].indexOf('header__nav_open') != -1 ? navigation.classList.remove('header__nav_open') : navigation.classList.add('header__nav_open');
-//     }
+if (window.innerWidth < 639) {    
+    document.querySelector("#menu__toggle").onclick = function change2() {
+        let off = document.querySelector('#menu__toggle')
+        if ((document.querySelector(".header__nav").style.visibility == "hidden") && (off.checked == true)) {
+            document.querySelector(".header__nav").style.visibility = "visible";
+        } else if ((document.querySelector(".header__nav").style.visibility == "visible") && (off.checked == false)) {
+            document.querySelector(".header__nav").style.visibility = "hidden";
+        }
+        else if (off.checked == true) {
+            document.querySelector(".header__nav").style.visibility = "visible";
+        } 
+    }
 
-document.querySelector(".header__nav__link").onclick = function change() {
-    document.querySelector(".header__nav").style.visibility = "hidden";
-    let off = document.querySelector('#menu__toggle');
-    off.checked = false
-}
-document.querySelector("#menu__toggle").onclick = function change2() {
-    let off = document.querySelector('#menu__toggle')
-    if ((document.querySelector(".header__nav").style.visibility = "hidden") && (off.checked = true)) {
-        document.querySelector(".header__nav").style.visibility = "visible";
-    } else if ((document.querySelector(".header__nav").style.visibility = "visible") && (off.checked = true)) {
-        document.querySelector(".header__nav").style.visibility = "hidden";
-    } else if (off.checked = true) {
-        document.querySelector(".header__nav").style.visibility = "visible";
-    } 
-}
 
-    // for (item of document.getElementsByClassName('header__nav__link')) {
-    //     item.onclick = function() {
-    //         const navigation = document.getElementsByClassName('header__nav')[0];
-    //         navigation.classList.remove('header__nav');
-    //     }
-    //     item.onclick = function() {
-    //         const navigation = document.getElementsByClassName('header__nav')[0];
-    //         navigation.classList.remove('header__nav');
-    //     }
-    //     item.onclick = function() {
-    //         const navigation = document.getElementsByClassName('header__nav')[0];
-    //         navigation.classList.remove('header__nav');
-    //     }
-    //     item.onclick = function() {
-    //         const navigation = document.getElementsByClassName('header__nav')[0];
-    //         navigation.classList.remove('header__nav');
-    //     }
-    //     item.onclick = function() {
-    //         const navigation = document.getElementsByClassName('header__nav')[0];
-    //         navigation.classList.remove('header__nav');
-    //     }
-    // }
+
+    for (item of document.querySelector('header__nav__link')) {
+        item.onclick = function() {
+            const navigation = document.querySelector('header__nav')[0];
+            navigation.style.visibility = "hidden";
+            let off = document.querySelector('#menu__toggle');    
+            off.checked = false
+        }
+    }
+}
