3f61b4d018814eb8d56b9c9d68606ce87bfc2a77
Mikhail8920 <other_vip@mail.ru>
2020-04-05 17:28:55 +0300

    add CubHUB

---
+++ CubHUB/first.js
@@ -0,0 +1,24 @@
+
+
+   
+    document.querySelector("#menu__toggle").onclick = function change2() {
+        
+        let off = document.querySelector('#menu__toggle')
+        if ((document.querySelector(".header__menu").style.visibility == "hidden") && (off.checked == true)) {
+            document.querySelector(".header__menu").style.visibility = "visible";
+        } else if ((document.querySelector(".header__menu").style.visibility == "visible") && (off.checked == false)) {
+            document.querySelector(".header__menu").style.visibility = "hidden";
+        }
+        else if (off.checked == true) {
+            document.querySelector(".header__menu").style.visibility = "visible";
+        } 
+    }
+
+    for (item of document.querySelectorAll('header__menu__nav')) {
+        item.onclick = function() {
+            const navigation = document.querySelector('header__menu');
+            navigation.style.visibility = "hidden";
+            let off = document.querySelector('#menu__toggle');    
+            off.checked = false
+        }
+    }
