4ddff49d3fd4f45eb6443ccbaddad608d36549f9
Mikhail8920 <other_vip@mail.ru>
2020-10-17 02:07:03 +0300

    started adding animation

--- Creative agency/js/one.js
+++ Creative agency/js/one.js
@@ -1,3 +1,35 @@
+const animElements = document.querySelectorAll('.animation__element');
+
+if (animElements.length > 0) {
+    window.addEventListener("scroll", animOnScroll);
+    function animOnScroll() {
+        for (let el = 0; el < animElements.length; el++) {
+            const animElem = animElements[el];
+            const animElemHeight = animElem.offsetHeight;
+            const animElemOffset = offset(animElem).top;
+            const animStart = 4;
+
+            let animElemPoint = window.innerHeight - animElemHeight / animStart;
+            if (animElemHeight > window.innerHeight) {
+                animElemPoint = window.innerHeight - window.innerHeight / animStart;
+            }
+
+            if ((pageYOffset > animElemOffset - animElemPoint) && pageYOffset < (animElemOffset + animElemHeight)) {
+                animElem.classList.add('active__element');
+            } else {
+                animElem.classList.remove('active__element');
+            }
+        }
+    }
+    function offset (elem) {
+        const rect = elem.getBoundingClientRect(),
+            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        return {top: rect.top + scrollTop, left: rect.left + scrollLeft} 
+    }
+    animOnScroll()
+}
+
 document.querySelector("#menu__toggle").onclick = function change2() {
     
     let off = document.querySelector('#menu__toggle')
@@ -20,3 +52,4 @@ for (item of document.querySelector('header__nav__link')) {
         off.checked = false
     }
 }
+
