ba8ecb556f5366f860d9df327f21a2356f10c3ca
Mikhalych <other_vip@mail.ru>
2021-01-08 13:04:27 +0300

    added one app and site to the portfolio.

--- Creative agency/js/one.js
+++ Creative agency/js/one.js
@@ -1,55 +1,60 @@
-const animElements = document.querySelectorAll('.animation__element');
+const animElements = document.querySelectorAll(".animation__element");
 
 if (animElements.length > 0) {
-    window.addEventListener("scroll", animOnScroll);
-    function animOnScroll() {
-        for (let el = 0; el < animElements.length; el++) {
-            const animElem = animElements[el];
-            const animElemHeight = animElem.offsetHeight;
-            const animElemOffset = offset(animElem).top;
-            const animStart = 4;
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let el = 0; el < animElements.length; el++) {
+      const animElem = animElements[el];
+      const animElemHeight = animElem.offsetHeight;
+      const animElemOffset = offset(animElem).top;
+      const animStart = 4;
 
-            let animElemPoint = window.innerHeight - animElemHeight / animStart;
-            if (animElemHeight > window.innerHeight) {
-                animElemPoint = window.innerHeight - window.innerHeight / animStart;
-            }
+      let animElemPoint = window.innerHeight - animElemHeight / animStart;
+      if (animElemHeight > window.innerHeight) {
+        animElemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
 
-            if ((pageYOffset > animElemOffset - animElemPoint) && pageYOffset < (animElemOffset + animElemHeight)) {
-                animElem.classList.add('active__element');
-            } else {
-                animElem.classList.remove('active__element');
-            }
-        }
+      if (
+        pageYOffset > animElemOffset - animElemPoint &&
+        pageYOffset < animElemOffset + animElemHeight
+      ) {
+        animElem.classList.add("active__element");
+      } else {
+        animElem.classList.remove("active__element");
+      }
     }
-    function offset (elem) {
-        const rect = elem.getBoundingClientRect(),
-            scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-        return {top: rect.top + scrollTop, left: rect.left + scrollLeft} 
-    }
-    animOnScroll()
+  }
+  function offset(elem) {
+    const rect = elem.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+  animOnScroll();
 }
 
 document.querySelector("#menu__toggle").onclick = function change2() {
-    
-    let off = document.querySelector('#menu__toggle')
-    if ((document.querySelector(".header__nav").style.visibility == "hidden") && (off.checked == true)) {
-        document.querySelector(".header__nav").style.visibility = "visible";
+  let off = document.querySelector("#menu__toggle");
+  if (
+    document.querySelector(".header__nav").style.visibility == "hidden" &&
+    off.checked == true
+  ) {
+    document.querySelector(".header__nav").style.visibility = "visible";
+  } else if (
+    document.querySelector(".header__nav").style.visibility == "visible" &&
+    off.checked == false
+  ) {
+    document.querySelector(".header__nav").style.visibility = "hidden";
+  } else if (off.checked == true) {
+    document.querySelector(".header__nav").style.visibility = "visible";
+  }
+};
 
-    } else if ((document.querySelector(".header__nav").style.visibility == "visible") && (off.checked == false)) {
-        document.querySelector(".header__nav").style.visibility = "hidden";
-    }
-    else if (off.checked == true) {
-        document.querySelector(".header__nav").style.visibility = "visible";
-    } 
+for (item of document.querySelector("header__nav__link")) {
+  item.onclick = function () {
+    const navigation = document.querySelector("header__nav");
+    navigation.style.visibility = "hidden";
+    let off = document.querySelector("#menu__toggle");
+    off.checked = false;
+  };
 }
-
-for (item of document.querySelector('header__nav__link')) {
-    item.onclick = function() {
-        const navigation = document.querySelector('header__nav');
-        navigation.style.visibility = "hidden";
-        let off = document.querySelector('#menu__toggle');    
-        off.checked = false
-    }
-}
-
--- laibas/js/one.js
+++ laibas/js/one.js
@@ -1,25 +1,25 @@
+document.querySelector("#menu__toggle").onclick = function change2() {
+  let off = document.querySelector("#menu__toggle");
+  if (
+    document.querySelector(".header__nav").style.visibility == "hidden" &&
+    off.checked == true
+  ) {
+    document.querySelector(".header__nav").style.visibility = "visible";
+  } else if (
+    document.querySelector(".header__nav").style.visibility == "visible" &&
+    off.checked == false
+  ) {
+    document.querySelector(".header__nav").style.visibility = "hidden";
+  } else if (off.checked == true) {
+    document.querySelector(".header__nav").style.visibility = "visible";
+  }
+};
 
-   
-    document.querySelector("#menu__toggle").onclick = function change2() {
-        
-        let off = document.querySelector('#menu__toggle')
-        if ((document.querySelector(".header__nav").style.visibility == "hidden") && (off.checked == true)) {
-            document.querySelector(".header__nav").style.visibility = "visible";
-        } else if ((document.querySelector(".header__nav").style.visibility == "visible") && (off.checked == false)) {
-            document.querySelector(".header__nav").style.visibility = "hidden";
-        }
-        else if (off.checked == true) {
-            document.querySelector(".header__nav").style.visibility = "visible";
-        } 
-    }
-
-
-
-    for (item of document.querySelector('header__nav__link')) {
-        item.onclick = function() {
-            const navigation = document.querySelector('header__nav');
-            navigation.style.visibility = "hidden";
-            let off = document.querySelector('#menu__toggle');    
-            off.checked = false
-        }
-    }
+for (item of document.querySelector("header__nav__link")) {
+  item.onclick = function () {
+    const navigation = document.querySelector("header__nav");
+    navigation.style.visibility = "hidden";
+    let off = document.querySelector("#menu__toggle");
+    off.checked = false;
+  };
+}
